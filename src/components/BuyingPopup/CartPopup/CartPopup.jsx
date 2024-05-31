c08f0862171d778f508bff99f7f8f88933447f16
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-05-31 10:13:17 +0200

    html scelet for buying PopUp

--- src/components/BuyingPopup/CartPopup/CartPopup.jsx
+++ src/components/BuyingPopup/CartPopup/CartPopup.jsx
@@ -10,17 +10,38 @@ const CartPopup = ({ onClose }) => {
         <div className={css.cartPopup}>
             <div className={css.cartPopupContent}>
                 <button className={css.closeButton} onClick={onClose}>×</button>
-                <h2>Корзина</h2>
+                <h1 className={css.myBasket}>Ваш кошик</h1>
                 {cart.length === 0 ? (
-                    <p>Ваша корзина пуста</p>
+                    <p className={css.emptyTitle}>Ваша корзина пуста</p>
                 ) : (
-                    <ul>
+                    <div className={css.honeySection}>
+                    <ul className={css.honeyList}>
                         {cart.map((item, index) => (
-                            <li key={index}>
-                                {item.weight} - {item.quantity} шт - {item.pricePerUnit * item.quantity} грн
+                            <li key={index} className={css.honeyItem}>
+                                {item.weight} - Кількість <button className={css.decreaseBtn}> - </button>{item.quantity} шт <button className={css.increaseBtn}> + </button> - {item.pricePerUnit * item.quantity} грн
                             </li>
                         ))}
                     </ul>
+                    <button className={css.deleteButton} type='button'>
+                        <svg>
+                   <use xlinkHref = ""></use> 
+                        </svg>
+                        </button>
+                    </div>
+                    <div className={css.modalButtonWrapper}>
+                        <div className={css.buttonBackWrapper}>
+                            <button className={css.buttonBack} type='button '>
+                                <svg>
+                                    <use xlinkHref = ""></use>
+                                </svg>
+                                Повернутися до покупок
+                            </button>
+                        </div>
+                        <div className={css.modalSubmitWrapper}>
+                            <p className={css.totalPrice}>Ціна</p>
+                            <button className={css.modalSubmitBtn} type="submit">Оформити замовлення</button>
+                        </div>
+                    </div>
                 )}
             </div>
         </div>
