8284a9674aeebbafe52cefa9cb468a8b7dee7499
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-05-31 12:03:38 +0200

    Updating with styles Buying CartPopup

--- src/components/BuyingPopup/CartPopup/CartPopup.jsx
+++ src/components/BuyingPopup/CartPopup/CartPopup.jsx
@@ -1,55 +1,71 @@
-
 import css from './CartPopup.module.css';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const CartPopup = ({ onClose }) => {
-    const cart = useSelector(state => state.cart);
+  const cart = useSelector(state => state.cart);
 
-    return (
-        <div className={css.cartPopup}>
-            <div className={css.cartPopupContent}>
-                <button className={css.closeButton} onClick={onClose}>×</button>
-                <h1 className={css.myBasket}>Ваш кошик</h1>
-                {cart.length === 0 ? (
-                    <p className={css.emptyTitle}>Ваша корзина пуста</p>
-                ) : (
-                    <div className={css.honeySection}>
-                    <ul className={css.honeyList}>
-                        {cart.map((item, index) => (
-                            <li key={index} className={css.honeyItem}>
-                                {item.weight} - Кількість <button className={css.decreaseBtn}> - </button>{item.quantity} шт <button className={css.increaseBtn}> + </button> - {item.pricePerUnit * item.quantity} грн
-                            </li>
-                        ))}
-                    </ul>
-                    <button className={css.deleteButton} type='button'>
-                        <svg>
-                   <use xlinkHref = ""></use> 
-                        </svg>
-                        </button>
+  return (
+    <div className={css.cartPopup}>
+      <div className={css.cartPopupContent}>
+        <button className={css.closeButton} onClick={onClose}>×</button>
+        <h2 className={css.myBasket}>Ваш кошик</h2>
+        {cart.length === 0 ? (
+          <p className={css.emptyTitle}>Ваша корзина пуста</p>
+        ) : (
+          <>
+            <div className={css.honeySection}>
+              <ul className={css.honeyList}>
+                {cart.map((item, index) => (
+                  <li key={index} className={css.honeyItem}>
+                    
+                    <div className={css.imgWrapper}>
+                        <img className={css.itemImg} src="../../../../public/img/honeyAcazion.png" alt="Мед акацієвий"  />
                     </div>
-                    <div className={css.modalButtonWrapper}>
-                        <div className={css.buttonBackWrapper}>
-                            <button className={css.buttonBack} type='button '>
-                                <svg>
-                                    <use xlinkHref = ""></use>
-                                </svg>
-                                Повернутися до покупок
-                            </button>
-                        </div>
-                        <div className={css.modalSubmitWrapper}>
-                            <p className={css.totalPrice}>Ціна</p>
-                            <button className={css.modalSubmitBtn} type="submit">Оформити замовлення</button>
-                        </div>
+                    <div className={css.descriptionWrapper}>
+                    
+                        <h3 className={css.itemTitle}>Мед акацієвий</h3>
+                        <p className={css.itemWeight}>{item.weight}</p> 
+                        <div className={css.itemQuantityWrapper}>
+                            <p className={css.itemQuantityText}>Кількість </p>
+                    <button className={css.decreaseBtn}> - </button>
+                    <p className={css.itemQuantity}>{item.quantity} шт </p>
+                    <button className={css.increaseBtn}> + </button> 
+                    - {item.pricePerUnit * item.quantity} грн</div>
                     </div>
-                )}
+                    
+                  </li>
+                ))}
+              </ul>
+              <button className={css.deleteButton} type='button'>
+                <svg>
+                  <use xlinkHref="#delete-icon"></use> 
+                </svg>
+              </button>
             </div>
-        </div>
-    );
+            <div className={css.modalButtonWrapper}>
+              <div className={css.buttonBackWrapper}>
+                <button className={css.buttonBack} type='button'>
+                  <svg>
+                    <use xlinkHref="#back-icon"></use>
+                  </svg>
+                  Повернутися до покупок
+                </button>
+              </div>
+              <div className={css.modalSubmitWrapper}>
+                <p className={css.totalPrice}>{cart.reduce((total, item ) => total + item.pricePerUnit * item.quantity, 0)} грн </p>
+                <button className={css.modalSubmitBtn} type="button">Оформити замовлення</button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 CartPopup.propTypes = {
-    onClose: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default CartPopup;
--- src/components/HoneyPopup/ProductDetails/ProductDetails.jsx
+++ src/components/HoneyPopup/ProductDetails/ProductDetails.jsx
@@ -10,56 +10,54 @@ import 'overlayscrollbars/styles/overlayscrollbars.css';
 const ProductDetails = () => {
   const dispatch = useDispatch();
   const product = useSelector(state => state.product);
-//   const cart = useSelector(state => state.cart);
 
   const handleWeightChange = (weight) => {
-      dispatch(setWeight(weight));
+    dispatch(setWeight(weight));
   };
 
   const handleQuantityChange = (quantity) => {
-      dispatch(setQuantity(quantity));
+    dispatch(setQuantity(quantity));
   };
 
   const handleAddToCart = () => {
-      const item = {
-          weight: product.weight,
-          quantity: product.quantity,
-          pricePerUnit: product.pricePerUnit * product.quantity,
-      };
-      dispatch(addToCart(item));
+    const item = {
+      weight: product.weight,
+      quantity: product.quantity,
+      pricePerUnit: product.pricePerUnit,
+    };
+    dispatch(addToCart(item));
   };
 
   const totalPrice = product.pricePerUnit * product.quantity;
 
   return (
-      <div className={css.productDetails}>
-          <h1 className={css.productTitle}>Мед акацієвий натуральний</h1>
-          <OverlayScrollbarsComponent className={css.containerScrollBar}>
-              <div className={css.scrollBar}>
-                  <p className={css.productDescription}>
-                      Кришталево прозорий рідкий світло-золотявого кольору мед з
-                      ніжним ароматом білої акації Смакові особливості: нейтральної
-                      солодкості, з кислинкою і ніжним ароматом цвітіння білої
-                      акації Корисні властивості: антисептична та протибактеріальна
-                      дія, нормалізація тиску, розширення судин, зміцнення
-                      імунітету, має гіпоалергенні властивості, містить велику
-                      концентрацію антиоксидантів Особливості: єдиний сорт меду,
-                      який зберігає рідкий стан і не має терміну придатності при
-                      правильному зберіганні, також єдиний сорт меду, який має
-                      гіпоалергенні властивості.
-                  </p>
-              </div>
-          </OverlayScrollbarsComponent>
-          <WeightOptions selectedWeight={product.weight} onWeightChange={handleWeightChange} />
-          {product.quantity !== undefined && (
-              <QuantitySelector quantity={product.quantity} onQuantityChange={handleQuantityChange} />
-          )}
-          <div className={css.productPurchase}>
-              <span className={css.productPrice}>{totalPrice} грн</span>
-          
-              <AddToCartButton onAddToCart={handleAddToCart} />
-          </div>
+    <div className={css.productDetails}>
+      <h1 className={css.productTitle}>Мед акацієвий натуральний</h1>
+      <OverlayScrollbarsComponent className={css.containerScrollBar}>
+        <div className={css.scrollBar}>
+          <p className={css.productDescription}>
+            Кришталево прозорий рідкий світло-золотявого кольору мед з
+            ніжним ароматом білої акації Смакові особливості: нейтральної
+            солодкості, з кислинкою і ніжним ароматом цвітіння білої
+            акації Корисні властивості: антисептична та протибактеріальна
+            дія, нормалізація тиску, розширення судин, зміцнення
+            імунітету, має гіпоалергенні властивості, містить велику
+            концентрацію антиоксидантів Особливості: єдиний сорт меду,
+            який зберігає рідкий стан і не має терміну придатності при
+            правильному зберіганні, також єдиний сорт меду, який має
+            гіпоалергенні властивості.
+          </p>
+        </div>
+      </OverlayScrollbarsComponent>
+      <WeightOptions selectedWeight={product.weight} onWeightChange={handleWeightChange} />
+      {product.quantity !== undefined && (
+        <QuantitySelector quantity={product.quantity} onQuantityChange={handleQuantityChange} />
+      )}
+      <div className={css.productPurchase}>
+        <span className={css.productPrice}>{totalPrice} грн</span>
+        <AddToCartButton onAddToCart={handleAddToCart} />
       </div>
+    </div>
   );
 };
 
