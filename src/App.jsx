c626bf3b604938ad1eea5e50fbe2ea0d9679943f
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-07-12 21:27:10 +0200

    Start of MainPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Product from "./components/HoneyPopup/Product/Product";
-import OrderPage from './pages/OrderPage/OrderPage'; // Замените на нужный путь импорта
+import OrderPage from './pages/OrderPage/OrderPage'; 
+import MainPage from './components/MainPage/MainPage'
 import "./App.css";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/" element={<Product />} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/product" element={<Product />} />
             <Route path="/order" element={<OrderPage />} />
           </Routes>
         </div>
--- src/components/OrderForm/Header/Header.jsx
+++ src/components/OrderForm/Header/Header.jsx
@@ -1,16 +1,22 @@
-
 import css from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className={css.orderHeader}>
       <div className={css.containerHeader}>
-        <a href="" className={css.orderLogo}>
+        <Link to="/" className={css.orderLogo}>
           <svg className={css.svgLogo}>
             <use xlinkHref="../../../public/icons/sprite.svg#logo"></use>
           </svg>
           <p className={css.beeAndHorn}>Бджола та Шершень</p>
-        </a>
+        </Link>
+        <nav className={css.nav}>
+          <Link to="/product" className={css.navLink}>Каталог</Link>
+          <a href="#about" className={css.navLink}>Про нас</a>
+          <a href="#reviews" className={css.navLink}>Відгуки</a>
+          <a href="#contacts" className={css.navLink}>Контакти</a>
+        </nav>
       </div>
     </div>
   );
