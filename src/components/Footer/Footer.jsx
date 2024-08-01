023a0114c9b16fc720d9ae41fc543b275da749a5
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-08-01 07:55:52 +0200

    starting of footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import css from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+   <>
+   <footer className={css.footer}>
+    <div className={css.footerContainer}>
+        <div className={css.footerContent}>
+            <a href=""></a>
+        </div>
+    </div>
+   </footer>
+   </>
+  )
+}
+
+export default Footer
